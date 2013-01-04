887141c55c6698a91316166b9711eaa6a995bd4c
Diego Allen <diegoallen@gmail.com>
2013-01-03 22:24:49 -0300

    list_movies function is done

--- movie_fetcher.py
+++ movie_fetcher.py
@@ -1,7 +1,40 @@
+from bs4 import BeautifulSoup
 import requests
 
-def fetch_movies():
-   pass 
+CINES_BASE_URL = 'http://www.cines.com.py'
+CARTELERA, NUEVOS = range(2)
+
+
+def list_movies(text, movies_type=CARTELERA):
+    """ Returns a dict with movie titles as keys and movie urls
+        as values. """
+
+    if movies_type == CARTELERA:
+        id = 'cuadro_lista_nuevos'
+    elif movies_type == NUEVOS:
+        id = 'cuadro_lista_cartelera'
+    else:
+        raise TypeError(
+            "expecting type of movie, got %s" % type(movie_type).__name__)
+
+    soup = BeautifulSoup(text)
+
+    movies = soup.find(id=id)
+    # some links are not needed
+    movies = movies.find_all('a')[:-3]
+
+    movie_titles = [a.text for a in movies]
+    movie_paths = [a['href'] for a in movies]
+
+    movies = dict(zip(movie_titles, movie_paths))
+    return movies
+
+
+def fetch_movie_data():
+    pass
+
 
 if __name__ == '__main__':
-    pass
+    r = requests.get(CINES_BASE_URL)
+    cartelera_movies = list_movies(r.text, movies_type=CARTELERA)
+    nuevos_movies = list_movies(r.text, movies_type=NUEVOS)
